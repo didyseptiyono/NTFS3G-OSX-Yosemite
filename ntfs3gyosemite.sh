adff9402692a08899083a1a3ff3756967a811d19
didyseptiyono <didy.sinchan@students.amikom.ac.id>
2015-03-30 11:36:46 +0700

    Create ntfs3gyosemite.sh

---
+++ ntfs3gyosemite.sh
@@ -0,0 +1,24 @@
+#Remove osxfuse if installed via homebrew:
+brew uninstall osxfuse
+
+#Install OSXFUSE
+brew install caskroom/cask/brew-cask  # installs Homebrew Cask
+brew cask install osxfuse # install osxfuse
+ 
+#Reboot (optional but recommended by osxfuse)
+ 
+#Install ntfs-3g via homebrew:
+brew update && brew install ntfs-3g
+ 
+#mount_ntfs:
+sudo mv /sbin/mount_ntfs /sbin/mount_ntfs.original
+sudo ln -s /usr/local/Cellar/ntfs-3g/2014.2.15/sbin/mount_ntfs /sbin/mount_ntfs
+ 
+#run this command
+ls -l /sbin/mount_ntfs*
+
+The output of the following should be as below:
+/sbin/mount_ntfs -> /usr/local/Cellar/ntfs-3g/2014.2.15/sbin/mount_ntfs
+/sbin/mount_ntfs.original -> /System/Library/Filesystems/ntfs.fs/Contents/Resources/mount_ntfs
+ 
+#Reboot and voila
